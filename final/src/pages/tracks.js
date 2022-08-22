0e31dac1537bd22d030eccb7a6809b56c74f2aa8
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2022-08-22 15:29:42 -0400

    workshop: display tracks for homepage

--- final/src/pages/tracks.js
+++ final/src/pages/tracks.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import { Layout } from '../components';
+import { useQuery, gql } from '@apollo/client';
+import { Layout, Loading } from '../components';
+import TrackCard from '../containers/track-card';
+
+/** TRACKS gql query to retreive all tracks */
+export const GET_TRACKS_FOR_HOMEPAGE = gql`
+  query GetTracksForHomepage {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
- * We display a grid of tracks fetched with useQuery with the TRACKS query
+ * We display a grid of tracks fetched with useQuery with the GET_TRACKS_FOR_HOMEPAGE query
  */
 const Tracks = () => {
-  return <Layout grid></Layout>;
+  const { loading, error, data } = useQuery(GET_TRACKS_FOR_HOMEPAGE);
+
+  if (error) {
+    return <p>ERROR: {error.message}</p>;
+  }
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (data) {
+    return (
+      <Layout grid>
+        {data.tracksForHome.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </Layout>
+    );
+  }
 };
 
 export default Tracks;
