29f4be58bd3e6a09eb1981d5e0fe0ac4936d712c
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2022-08-22 12:49:03 -0400

    workshop: initialize ApolloClient (uri pointing to Odyssey link for now)

--- final/src/index.js
+++ final/src/index.js
@@ -4,12 +4,21 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
 
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://odyssey-lift-off-server.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <GlobalStyles />
-      <Pages />
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <GlobalStyles />
+        <Pages />
+      </Router>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
