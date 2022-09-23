bde2d1754bce9dd2a23bb198ff891d8d6149a2e5
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2022-09-23 12:05:14 -0400

    impr: add boilerplate for ApolloClient and ApolloProvider initialization

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'TODO',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
